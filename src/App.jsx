7da1a8f66158ff527ce297fa15725124daada11e
Malo Caparros <103109650+MaloCaparros@users.noreply.github.com>
2026-05-31 17:05:51 +0200

    Feat : Ajout de la page dashboard pour utiliser la modal et le système de stockage

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
+import DashboardPage from './pages/DashboardPage';
 
 const navElements = [
   { label: "About", path: "/about" },
   { label: "Projects", path: "/#projects" },
   { label: "Contacts", path: "/#contacts" },
+  { label: "Dashboard", path: "/dashboard" },
 ];
 
 function App() {
@@ -17,6 +19,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
         </Routes>
       </main>
     </BrowserRouter>
